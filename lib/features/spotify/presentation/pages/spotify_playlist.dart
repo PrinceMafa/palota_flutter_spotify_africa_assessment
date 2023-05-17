ddd08ec8b99355d64f5943cf7d5c957dc16318d3
Prince Mafa <mafaprince8@.com>
2023-05-17 15:44:27 +0200

    Testing_to_see_if_it_works_:)

--- lib/features/spotify/presentation/pages/spotify_playlist.dart
+++ lib/features/spotify/presentation/pages/spotify_playlist.dart
@@ -1,11 +1,69 @@
 import 'package:flutter/material.dart';
 
-//TODO: complete this page - you may choose to change it to a stateful widget if necessary
-class SpotifyPlaylist extends StatelessWidget {
-  const SpotifyPlaylist({Key? key}) : super(key: key);
+import 'package:spotify_api/spotify_api.dart';
+
+class SpotifyPlaylist extends StatefulWidget {
+  final String playlistId;
+
+  const SpotifyPlaylist({
+    Key? key,
+    required this.playlistId,
+  }) : super(key: key);
+
+  @override
+  State<SpotifyPlaylist> createState() => _SpotifyPlaylistState();
+}
+
+class _SpotifyPlaylistState extends State<SpotifyPlaylist> {
+  late Future<PlaylistDetails> playlistDetails;
+
+  @override
+  void initState() {
+    super.initState();
+    playlistDetails = SpotifyAPI.getPlaylistDetails(playlistId);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Playlist Name'),
+      ),
+      body: FutureBuilder(
+        future: playlistDetails,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              PlaylistDetails playlistDetails = snapshot.data;
+              return ListView(
+                children: [
+                  Image.network(playlistDetails.imageUrl),
+                  SizedBox(height: 8.0),
+                  Text(playlistDetails.name),
+                  SizedBox(height: 8.0),
+                  Text(playlistDetails.description),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Number of followers: ${playlistDetails.numberOfFollowers}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 8.0),
+                  TracksList(tracks: playlistDetails.tracks),
+                  ArtistsList(artists: playlistDetails.artists),
+                ],
+              );
+            } else {
+              return Center(
+                child: Text('No playlist found'),
+              );
+            }
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 }
